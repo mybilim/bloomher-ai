a1a5871c758215ae06e461fa8b2da6e1290c2c8d
mybilim <mybilim20@gmail.com>
2026-07-25 16:53:35 +0000

    Milestone 151-170: Premium subscription

---
+++ components/payment/PremiumCard.tsx
@@ -0,0 +1,13 @@
+
+export default function PremiumCard(){
+
+return(
+<div>
+<h2>
+BloomHer Premium
+</h2>
+</div>
+)
+
+}
+
---
+++ lib/payment/status.ts
@@ -0,0 +1,7 @@
+
+export function checkPremium(
+user:any
+){
+return user?.premium===true;
+}
+
